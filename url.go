02bc909a8bb40a62eddc90256f74b95ee172dc1e
bytedream <bytedream@protonmail.com>
2022-07-23 16:23:15 +0200

    Use Location http header to get beta url from classic url (crunchy-labs/crunchy-cli#22)

--- url.go
+++ url.go
@@ -2,6 +2,7 @@ package crunchyroll
 
 import (
 	"fmt"
+	"net/http"
 )
 
 // ExtractEpisodesFromUrl extracts all episodes from an url.
@@ -80,30 +81,72 @@ func (c *Crunchyroll) ExtractEpisodesFromUrl(url string, audio ...LOCALE) ([]*Ep
 // ParseUrl parses the given url into a series or episode.
 // The returning episode is a slice because non-beta urls have the same episode with different languages.
 func (c *Crunchyroll) ParseUrl(url string) (*Series, []*Episode, error) {
+	return parseUrl(c, url, false)
+}
+
+func parseUrl(crunchy *Crunchyroll, url string, recursive bool) (*Series, []*Episode, error) {
 	if seriesId, ok := ParseBetaSeriesURL(url); ok {
-		series, err := SeriesFromID(c, seriesId)
+		series, err := SeriesFromID(crunchy, seriesId)
 		if err != nil {
 			return nil, nil, err
 		}
 		return series, nil, nil
 	} else if episodeId, ok := ParseBetaEpisodeURL(url); ok {
-		episode, err := EpisodeFromID(c, episodeId)
+		episode, err := EpisodeFromID(crunchy, episodeId)
 		if err != nil {
 			return nil, nil, err
 		}
 		return nil, []*Episode{episode}, nil
 	} else if seriesName, ok := ParseVideoURL(url); ok {
-		video, err := c.FindVideoByName(seriesName)
+		if recursive {
+			return nil, nil, fmt.Errorf("unexpected recursion for url %s", url)
+		}
+
+		oldRedirect := crunchy.Client.CheckRedirect
+		crunchy.Client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+		defer func() {
+			crunchy.Client.CheckRedirect = oldRedirect
+		}()
+		resp, err := crunchy.request(url)
 		if err != nil {
 			return nil, nil, err
 		}
-		return video.(*Series), nil, nil
+		if redirectUrl := resp.Header.Get("Location"); redirectUrl != "" {
+			return parseUrl(crunchy, redirectUrl, true)
+		} else {
+			video, err := crunchy.FindVideoByName(seriesName)
+			if err != nil {
+				return nil, nil, err
+			}
+			return video.(*Series), nil, nil
+		}
 	} else if seriesName, title, _, _, ok := ParseEpisodeURL(url); ok {
-		episodes, err := c.FindEpisodeByName(seriesName, title)
+		if recursive {
+			return nil, nil, fmt.Errorf("unexpected recursion for url %s", url)
+		}
+
+		oldRedirect := crunchy.Client.CheckRedirect
+		crunchy.Client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+		defer func() {
+			crunchy.Client.CheckRedirect = oldRedirect
+		}()
+		resp, err := crunchy.request(url)
 		if err != nil {
 			return nil, nil, err
 		}
-		return nil, episodes, nil
+		if redirectUrl := resp.Header.Get("Location"); redirectUrl != "" {
+			return parseUrl(crunchy, redirectUrl, true)
+		} else {
+			episodes, err := crunchy.FindEpisodeByName(seriesName, title)
+			if err != nil {
+				return nil, nil, err
+			}
+			return nil, episodes, nil
+		}
 	} else {
 		return nil, nil, fmt.Errorf("invalid url %s", url)
 	}
